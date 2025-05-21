e4129489500134da4d6b99f4f74f5c1434c1f490
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-05-21 12:42:57 +0530

    Update caption_manage.py

--- MeshRenameBot/core/caption_manage.py
+++ MeshRenameBot/core/caption_manage.py
@@ -23,21 +23,21 @@ def get_inline_keyboard(translator: Translator) -> InlineKeyboardMarkup:
 async def set_caption(_: Client, msg: Message):
     udb = UserDB()
     user_id = msg.from_user.id
-    user_locale = udb.get_var("locale", user_id)
+    user_locale = await udb.get_var("locale", user_id)
     translator = Translator(user_locale)
 
     parts = msg.text.split(" ", 1)
     caption = parts[1] if len(parts) == 2 else ""
 
     if not caption:
-        old_caption = udb.get_var("caption", msg.from_user.id) or ""
+        old_caption = await udb.get_var("caption", msg.from_user.id) or ""
         message_text = (
             translator.get("CAPTION_SET", caption=old_caption)
             if old_caption
             else translator.get("CAPTION_NOT_SET")
         ) + f"\n\n{translator.get('CAPTION_FOOT_NOTE', )}"
     else:
-        udb.set_var("caption", caption, msg.from_user.id)
+        await udb.set_var("caption", caption, msg.from_user.id)
         message_text = (
             translator.get("CAPTION_SET", caption=caption)
             + f"\n\n{translator.get('CAPTION_FOOT_NOTE')}"
@@ -51,10 +51,10 @@ async def set_caption(_: Client, msg: Message):
 async def del_caption(_: Client, msg: CallbackQuery):
     udb = UserDB()
     user_id = msg.from_user.id
-    user_locale = udb.get_var("locale", user_id)
+    user_locale = await udb.get_var("locale", user_id)
     translator = Translator(user_locale)
 
-    udb.set_var("caption", "", msg.from_user.id)
+    await udb.set_var("caption", "", msg.from_user.id)
     await msg.message.edit_text(
         translator.get("CAPTION_NOT_SET")
         + f"\n\n{translator.get('CAPTION_FOOT_NOTE')}",
