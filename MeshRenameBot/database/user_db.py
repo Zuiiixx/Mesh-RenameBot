b8553321d340c34ec110235334a59c4ac9f72a95
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-05-21 17:50:16 +0530

    Update user_db.py

--- MeshRenameBot/database/user_db.py
+++ MeshRenameBot/database/user_db.py
@@ -1,10 +1,6 @@
 from ..core.get_config import get_var
-import asyncio
 
-async def get_user_db():
-    is_mongo = await get_var("IS_MONGO")
-    if is_mongo:
-        from .mongo_impl import UserDB as UserDBImpl
-    else:
-        from .postgres_impl import UserDB as UserDBImpl
-    return UserDBImpl
+if get_var("IS_MONGO"):
+    from .mongo_impl import UserDB
+else:
+    from .postgres_impl import UserDB
