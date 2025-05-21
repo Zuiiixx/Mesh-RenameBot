63200e1cd5a7111735800cc8ee3a605443e4da59
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-05-21 12:31:37 +0530

    Update mongo_impl.py

--- MeshRenameBot/database/mongo_impl.py
+++ MeshRenameBot/database/mongo_impl.py
@@ -1,13 +1,10 @@
-from .mongo_db import MongoDB
-from ..core.get_config import get_var
 import os
 import json
 from typing import Union
+from pymongo import MongoClient
 
-class UserDB(MongoDB):
-    shared_users = {}
 
-    # Add all these constants
+class MongoDB(MongoClient):
     MODE_SAME_AS_SENT = 0
     MODE_AS_DOCUMENT = 1
     MODE_AS_GMEDIA = 2
@@ -16,113 +13,69 @@ class UserDB(MongoDB):
 
     def __init__(self, dburl=None):
         if dburl is None:
-            dburl = os.environ.get("DATABASE_URL", None)
+            dburl = os.environ.get("DATABASE_URL")
             if dburl is None:
+                from MeshRenameBot.core.get_config import get_var  # Lazy import to avoid circular errors
                 dburl = get_var("DATABASE_URL")
         super().__init__(dburl)
+        self._db = self.get_database()
 
-    def get_var(self, var: str, user_id: int) -> Union[None, str]:
-        user_id = str(user_id)
-        db = self._db
-        users = db.mesh_rename
-
-        user = users.find_one({"user_id": user_id})
+    def get_user_var(self, var: str, user_id: int) -> Union[None, str]:
+        user = self._db.mesh_rename.find_one({"user_id": str(user_id)})
         if user:
-            jdata = json.loads(user["json_data"])
+            jdata = json.loads(user.get("json_data", "{}"))
             return jdata.get(var)
-        else:
-            return None
+        return None
 
-    def set_var(self, var: str, value: Union[int, str], user_id: int) -> None:
+    def set_user_var(self, var: str, value: Union[int, str], user_id: int) -> None:
+        users = self._db.mesh_rename
         user_id = str(user_id)
-        db = self._db
-        users = db.mesh_rename
 
         user = users.find_one({"user_id": user_id})
         if user:
-            jdata = json.loads(user["json_data"])
+            jdata = json.loads(user.get("json_data", "{}"))
             jdata[var] = value
             users.update_one({"_id": user["_id"]}, {"$set": {"json_data": json.dumps(jdata)}})
         else:
-            jdata = {var: value}
             users.insert_one({
                 "user_id": user_id,
-                "json_data": json.dumps(jdata),
-                "file_choice": 0,
+                "json_data": json.dumps({var: value}),
+                "file_choice": self.MODE_SAME_AS_SENT,
                 "thumbnail": None
             })
 
     def get_thumbnail(self, user_id: int) -> Union[str, bool]:
-        user_id = str(user_id)
-        db = self._db
-        users = db.mesh_rename
+        user = self._db.mesh_rename.find_one({"user_id": str(user_id)})
+        if user and user.get("thumbnail"):
+            user_dir = os.path.join(os.getcwd(), 'userdata', str(user_id))
+            os.makedirs(user_dir, exist_ok=True)
 
-        row = users.find_one({"user_id": user_id})
-        if row:
-            if row["thumbnail"] is None:
-                return False
-            else:
-                path = os.path.join(os.getcwd(), 'userdata')
-                if not os.path.exists(path):
-                    os.mkdir(path)
+            path = os.path.join(user_dir, "thumbnail.jpg")
+            with open(path, "wb") as f:
+                f.write(user["thumbnail"])
+            return path
+        return False
 
-                path = os.path.join(path, user_id)
-                if not os.path.exists(path):
-                    os.mkdir(path)
-
-                path = os.path.join(path, "thumbnail.jpg")
-                with open(path, "wb") as rfile:
-                    rfile.write(row["thumbnail"])
-
-                return path
-        else:
-            return False
-
-    def set_thumbnail(self, thumbnail: bytes, user_id: int) -> bool:
-        user_id = str(user_id)
-        db = self._db
-        users = db.mesh_rename
-
+    def set_thumbnail(self, thumbnail: Union[str, bytes], user_id: int) -> bool:
         if isinstance(thumbnail, str):
             with open(thumbnail, "rb") as f:
                 thumbnail = f.read()
 
-        user = users.find_one({"user_id": user_id})
-        if user:
-            users.update_one({"user_id": user_id}, {"$set": {"thumbnail": thumbnail}})
-        else:
-            users.insert_one({
-                "user_id": user_id,
-                "thumbnail": thumbnail,
-                "json_data": json.dumps({}),
-                "file_choice": 0
-            })
-
+        user_id = str(user_id)
+        self._db.mesh_rename.update_one(
+            {"user_id": user_id},
+            {"$set": {"thumbnail": thumbnail}},
+            upsert=True
+        )
         return True
 
     def set_mode(self, mode: int, user_id: int) -> None:
-        user_id = str(user_id)
-        db = self._db
-        users = db.mesh_rename
-
-        user = users.find_one({"user_id": user_id})
-        if user:
-            users.update_one({"user_id": user_id}, {"$set": {"file_choice": mode}})
-        else:
-            users.insert_one({
-                "user_id": user_id,
-                "file_choice": mode,
-                "thumbnail": None,
-                "json_data": json.dumps({})
-            })
+        self._db.mesh_rename.update_one(
+            {"user_id": str(user_id)},
+            {"$set": {"file_choice": mode}},
+            upsert=True
+        )
 
     def get_mode(self, user_id: int) -> int:
-        user_id = str(user_id)
-        db = self._db
-        users = db.mesh_rename
-
-        user = users.find_one({"user_id": user_id})
-        if user:
-            return user.get("file_choice", self.MODE_SAME_AS_SENT)
-        else:
-            return self.MODE_SAME_AS_SENT
+        user = self._db.mesh_rename.find_one({"user_id": str(user_id)})
+        return user.get("file_choice", self.MODE_SAME_AS_SENT) if user else self.MODE_SAME_AS_SENT
