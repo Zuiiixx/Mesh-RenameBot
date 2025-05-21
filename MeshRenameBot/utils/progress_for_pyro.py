6c1adea9b966d12b6ea2563edc7ea3d6b64394fa
Zuiiixx <b7ctx5d84qx@gmail.com>
2025-05-21 12:45:12 +0530

    Update progress_for_pyro.py

--- MeshRenameBot/utils/progress_for_pyro.py
+++ MeshRenameBot/utils/progress_for_pyro.py
@@ -23,7 +23,7 @@ async def progress_for_pyrogram(
 ):
     now = time.time()
     diff = now - start
-    
+
     # too early to update the progress
     if diff < 1:
         return
@@ -32,27 +32,29 @@ async def progress_for_pyrogram(
         raise StopTransmission()
 
     if round(diff % time_out) == 0 or current == total:
-    
-        # if round(current / total * 100, 0) % 5 == 0:
+
         percentage = current * 100 / total
         elapsed_time = round(diff)
         speed = current / elapsed_time
         time_to_completion = round((total - current) / speed)
         estimated_total_time = elapsed_time + time_to_completion
 
-        elapsed_time = human_readable_timedelta(elapsed_time)
-        estimated_total_time = human_readable_timedelta(estimated_total_time)
+        elapsed_time_str = human_readable_timedelta(elapsed_time)
+        estimated_total_time_str = human_readable_timedelta(estimated_total_time)
+
+        completed_str = await get_var("COMPLETED_STR")
+        remaining_str = await get_var("REMAINING_STR")
 
         progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join([get_var("COMPLETED_STR") for _ in range(math.floor(percentage / 10))]),
-            ''.join([get_var("REMAINING_STR") for _ in range(10 - math.floor(percentage / 10))]),
+            ''.join([completed_str for _ in range(math.floor(percentage / 10))]),
+            ''.join([remaining_str for _ in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
             human_readable_bytes(current),
             human_readable_bytes(total),
             human_readable_bytes(speed),
-            estimated_total_time if estimated_total_time != '' else "0 seconds"
+            estimated_total_time_str if estimated_total_time_str != '' else "0 seconds"
         )
         try:
             if not message.photo:
